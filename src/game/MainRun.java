d59f648d33685cadd7ee561122fb5e679b810a60
nescude <nescude042@hotmail.com>
2018-09-05 15:51:09 -0300

    comments

--- src/game/MainRun.java
+++ src/game/MainRun.java
@@ -11,13 +11,19 @@ import vista.Window;
 public class MainRun {
 
 
+    
     public static void main(String[] args) {
+        /**
+         * INIT VARIABLES
+         */
         GameMaster gm = new GameMaster();
-        Deck mazo = new Deck();
         IWindow vis = new Window();
         vis.arranca();
         Player user = new User(200,3,3);
         Player ia = new IA(200,3,3);
+        /**
+         * REFERENCES SET'S
+         */
         user.setVis(vis);
         ia.setVis(vis);
         gm.setIa(ia);
@@ -26,6 +32,9 @@ public class MainRun {
         vis.setIa(ia);
         vis.setUser(user);
         vis.setGm(gm);
+        /**
+         * START
+         */
         gm.empezar();
     }
 }
